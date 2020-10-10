e79704e11f651179427f278e031c3bec3f336a26
Aiaksers <72662835+Aiaksers@users.noreply.github.com>
2020-10-10 23:37:28 +0300

    Add files via upload

---
+++ ese.py
@@ -0,0 +1,21 @@
+print("Silence is gold")
+
+a=input()
+z=input()
+print(int(a)+int(z))
+print(int(a)*int(z))
+y=input()
+o=input()
+u=input()
+p=int(y)*2
+r=int(o)-3
+c=int(u)**2
+print(int(p)+int(r)+int(c))
+print('''
+AAAAAAAA
+A      A
+A      A
+A      A
+A      A
+AAAAAAAA
+''')
