3d93978c1203d146736a00aa294ceb6eaec524e9
Aiaksers <mrrdogerman@gmail.com>
2020-10-19 20:41:32 +0300

    Initial commit.

---
+++ Homework/18.10.2020 домашка/Задание 1/Numbers.py
@@ -0,0 +1,6 @@
+print(" Напишите любое число от -10 до 10, и программа его увеличит на 5")
+numOne=int(input())
+if numOne > -10 and numOne < 10:
+    print(numOne+5)
+else:
+   print(" иди туда изучать цифры https://pedsovet.su/load/240 или там https://www.youtube.com/watch?v=EBv4v1ODG1E&feature=emb_logo")
---
+++ Homework/18.10.2020 домашка/Задание 2/Месяц.py
@@ -0,0 +1,32 @@
+print("Введите номер месяца")
+number=int(input())
+if number >= 1 and number <= 12:
+    if number==1:
+        print ('Это январь')
+    if number == 2:
+        print('Это февраль')
+    if number == 3:
+        print('Это март')
+    if number==4:
+        print ('Это апрель')
+    if number == 5:
+        print('Это май')
+    if number == 6:
+        print('Это июнь')
+    if number == 7:
+        print('Это июль')
+    if number == 8:
+        print('Это август')
+    if number == 9:
+        print('Это сентябрь')
+    if number == 10:
+        print('Это октябрь')
+    if number == 11:
+        print('Это ноябрь')
+    if number == 12:
+        print('Это декабрь')
+else:
+     print('Месяцы у коста офигели, у них на тринадцатого собутыльника водки не хватит')
+
+
+
