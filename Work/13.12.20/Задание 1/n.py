3a62dbc0043c59713724e0b8e74a8eb7a8421d91
Aiaksers <mrrdogerman@gmail.com>
2020-12-13 17:00:43 +0300

    v12

---
+++ Work/13.12.20/Задание 1/n.py
@@ -0,0 +1,15 @@
+kaloda = ["Пиковый Туз", "Пика 2", "Пика 3", "Пика 4", "Пика 5", "Пика 6", " Пика 7", "Пика 8", "Пика 9"]
+print(kaloda)
+print("--------------------------------------------------------------------------------------------")
+firstpart = kaloda[0:4]
+print("Первая пара- ", firstpart)
+print()
+secondpart = kaloda[4:10]
+print("Вторая пара- ", secondpart)
+print("----------------------------------------------------------------------------------------")
+firstpart1 = firstpart[0:2]
+firstpart2 = firstpart[2:4]
+secondpart1 = secondpart[0:3]
+secondpart2 = secondpart[3:6]
+print("-------------------------------------------------------------------------------------------------")
+print(firstpart1+secondpart1+firstpart2+secondpart2)
